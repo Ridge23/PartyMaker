3665b468a2f511ce7f0010639badff2f01db9c98
Pavel Khrebto <ridgeweber@gmail.com>
2015-02-02 22:58:29 +0200

    cities and countries db seeds from files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ActiveRecord::Base.connection.execute('TRUNCATE ' + Role.table_name + ' RESTART IDENTITY')
+
 Role.find_or_create_by({name: 'Artist / DJ', plain_name: 'artist_dj'})
 Role.find_or_create_by({name: 'Promoter / Club', plain_name: 'promoter_club'})
+
+ActiveRecord::Base.connection.execute('TRUNCATE ' + Genre.table_name + ' RESTART IDENTITY')
 
 genres = Genre.create([
   { name: 'Breaks', enabled: true },
@@ -32,3 +36,30 @@ genres = Genre.create([
   { name: 'Trance', enabled: true },
   { name: 'Hardcore / Hard Techno', enabled: true }
 ])
+
+ActiveRecord::Base.connection.execute('TRUNCATE ' + Country.table_name + ' RESTART IDENTITY')
+
+countries_csv_path = File.join(Rails.root, 'db', 'GeoIPCountryWhois.csv')
+
+File.open(countries_csv_path).each do |line|
+  country_array = line.split(',')
+  if !Country.find_by_iso_name(country_array[4].gsub(/\s|"|'/, ''))
+    Country.create({ name: country_array[5].gsub(/\s|"|'/, ''), iso_name: country_array[4].gsub(/\s|"|'/, '')})
+  end
+end
+
+ActiveRecord::Base.connection.execute('TRUNCATE ' + City.table_name + ' RESTART IDENTITY')
+
+cities_csv_path = File.join(Rails.root, 'db', 'GeoLiteCity-Location.csv')
+
+File.open(cities_csv_path).each do |line|
+  if line
+    city_array = line.split(',')
+    if city_array[1] and city_array[3]
+      city_country = Country.find_by_iso_name(city_array[1].gsub(/\s|"|'/, ''))
+      if city_country
+        City.create({ name: city_array[3].gsub(/\s|"|'/, ''), country_id: city_country.id})
+      end
+    end
+  end
+end
